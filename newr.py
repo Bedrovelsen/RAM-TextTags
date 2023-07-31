d5bded5c3537ad0246570811055487170b851f0a
Lennie Budgell <868085+Bedrovelsen@users.noreply.github.com>
2023-07-31 22:39:48 +0000

    Cleaned up

--- newr.py
+++
@@ -1,9 +0,0 @@
-from gradio_client import Client
-
-client = Client("https://xinyu1205-recognize-anything.hf.space/")
-result = client.predict(
-				"https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png",	# str (filepath or URL to image) in 'parameter_17' Image component
-				"Howdy!",	# str  in 'User Specified Tags (Optional, separated by comma)' Textbox component
-				fn_index=3
-)
-print(result)
